588193c6f2e649dd3766434a12773b02b7f1541c
Higor Henrique <89036213+HigorHenriq@users.noreply.github.com>
2021-08-22 17:37:44 -0300

    Delete Promoção.js

--- FaseTRES/Exercicios AlgoritimoFASETRES/Promoção.js
+++
@@ -1,7 +0,0 @@
-/*
- É muito comum vermos promoções em mercados e shoppings
-relacionadas ao valor das compras. Se a Alice fez suas compras em um
-shopping cuja promoção era que a cada R$ 190,00 em compras ela
-ganhava um cupom, quantos cupons ela ganhou? */
-
-//Qual quantidade que alice gastou?
