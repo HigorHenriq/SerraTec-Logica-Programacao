cf3a45486da47c589033288aaa39351edfb22f62
Higor Henrique <89036213+HigorHenriq@users.noreply.github.com>
2021-08-22 19:41:45 -0300

    Update Programa de fidelidade.js

--- FaseTRES/Exercicios AlgoritimoFASETRES/Programa de fidelidade.js
+++ FaseTRES/Exercicios AlgoritimoFASETRES/Programa de fidelidade.js
@@ -6,28 +6,4 @@ corresponde a 1 ponto.
 */
 const prompt = require("prompt-sync")()
 
-var pergunta = 1//prompt('Verifique seus pontos em cupons, digite o número dele: ')
-
-switch(pergunta){
-    case 0:
-    console.log('Você tem 0 cupons');
-        break;
-    case 1:
-    console.log('Você tem 1 cupons');
-        break;
-    case 2:
-    console.log('Você tem 2 cupons');
-        break;
-    case 3:
-    console.log('Você tem 3 cupons');
-        break;
-    case 4:
-    console.log('Você tem 4 cupons');
-        break;
-    case 5:
-    console.log('Você tem 5 cupons');
-        break;
-    default:
-        console.log('Não idetificamos sua resposta');
-        break;
-}
+var pergunta = prompt('Verifique seus pontos em cupons, digite o número dele: ')
